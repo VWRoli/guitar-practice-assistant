32a2919d1cb9f775dc88a113e19dc8f81a2cd429
VWRoli <fustroli@gmail.com>
2021-06-08 15:27:10 +0200

    server side setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
-console.log('Mern app');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config({ silent: true });
+
+//App init
+const app = express();
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const port = process.env.PORT;
+
+//Connect to DB
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => console.log(`Server is up on port ${port}`));
+  })
+  .catch((error) => console.log(error.message));
