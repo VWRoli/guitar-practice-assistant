f9e4ae31745831251ea90d2c6e288476812c87db
VWRoli <fustroli@gmail.com>
2021-06-08 14:32:57 +0200

    MERN init

--- src/js/controller.js
+++
@@ -1,6 +0,0 @@
-//application logic
-//bridge between model and view, which dont know about each other
-//THIS IS A MODULE
-
-//controls all user actions
-//dispatches actions to model and view
--- src/js/views/views.js
+++
@@ -1,3 +0,0 @@
-//presentation logic
-//this is a class,
-//render practice items to screen
