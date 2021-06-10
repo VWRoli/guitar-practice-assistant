dd1be85cabf98d88908d7660bcd28e34d705b684
VWRoli <fustroli@gmail.com>
2021-06-10 15:03:56 +0200

    add metronome

--- client/src/components/MainContent/MainContent.js
+++ client/src/components/MainContent/MainContent.js
@@ -1,26 +1,36 @@
+import Metronome from '@kevinorriss/react-metronome';
+
 const MainContent = () => {
   return (
-    <section className="home content">
-      <h2>Welcome to your Guitar Practice Assistant</h2>
-      <p>Here you will find everything you need for your practice sessions.</p>
-      <h3>Practice Items</h3>
-      <p>
-        You can add your practice items on the left. Set a title and a
-        <br />
-        practice duration. You can choose between a song or an excercise.
-      </p>
-      <h3>Metronome</h3>
-      <p>
-        The most important thing for any musician is keeping time. You can
-        <br />
-        practice with a metronome under the Metronome tab.
-      </p>
-      <h3>Fretboard</h3>
-      <p>
-        Every guitarist should know the notes on the fretboard. Practice
-        <br />
-        your notes with a fun game under the Fretboard tab.
-      </p>
+    <section className="metronome-container">
+      <Metronome
+        frequency={880}
+        volume={1}
+        playPauseStyle={{
+          color: '#4285f4',
+          backgroundColor: '#fff',
+          height: '9rem',
+          width: '9rem',
+          fontSize: '6rem',
+        }}
+        bpmStyle={{ color: '#4285f4', marginTop: '1.5rem' }}
+        plusStyle={{
+          backgroundColor: '#4285f4',
+          fontSize: '1.5rem',
+          padding: '5px',
+          height: 'auto',
+        }}
+        minusStyle={{
+          backgroundColor: '#4285f4',
+          fontSize: '1.5rem',
+          padding: '5px',
+          height: 'auto',
+        }}
+        trackStyle={{ height: '8px', backgroundColor: '#4285f4' }}
+        railStyle={{ height: '8px' }}
+        handleStyle={{ height: '18px', width: '18px' }}
+        sliderStyle={{ minWidth: '250px' }}
+      />
     </section>
   );
 };
