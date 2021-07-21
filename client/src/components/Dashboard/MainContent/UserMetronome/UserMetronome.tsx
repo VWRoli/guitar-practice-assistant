44b301f206578c8fbbb20711b28f88ef8e8ab96d
VWRoli <fustroli@gmail.com>
2021-07-21 15:26:59 +0200

    Issue #17 BUG/Make the dashboard responsive

--- client/src/components/Dashboard/MainContent/UserMetronome/UserMetronome.tsx
+++ client/src/components/Dashboard/MainContent/UserMetronome/UserMetronome.tsx
@@ -1,9 +1,18 @@
 // @ts-ignore
 import Metronome from '@kevinorriss/react-metronome';
+import { useState } from 'react';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
 
 const UserMetronome = (): JSX.Element => {
+  const [isOpen, setisOpen] = useState(false);
   return (
-    <section className="metronome-container">
+    <section
+      className={
+        isOpen ? 'metronome-container' : 'metronome-container hide-slider'
+      }>
+      <button className="metronome-slider" onClick={() => setisOpen(!isOpen)}>
+        {isOpen ? <FaChevronDown /> : <FaChevronUp />}
+      </button>
       <Metronome
         frequency={880}
         volume={1}
